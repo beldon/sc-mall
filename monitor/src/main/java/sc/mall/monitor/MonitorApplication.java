b0c246b4abe77896e6fbec72dc69846fd46468fb
beldon <1047325205@qq.com>
2021-07-10 17:58:11 +0800

    update

--- monitor/src/main/java/sc/mall/monitor/MonitorApplication.java
+++ monitor/src/main/java/sc/mall/monitor/MonitorApplication.java
@@ -1,9 +1,11 @@
 package sc.mall.monitor;
 
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
+@EnableAdminServer
 public class MonitorApplication {
     public static void main(String[] args) {
         SpringApplication.run(MonitorApplication.class, args);
